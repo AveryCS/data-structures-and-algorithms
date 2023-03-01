207c431d32a644a1cc1f270779416324865df75a
AveryCS <averydcs@gmail.com>
2023-03-01 14:33:54 -0500

    Adding recursive solution for Power of Two

---
+++ src/com/practice/recursionproblems/PowerOfTwo.java
@@ -0,0 +1,34 @@
+package com.practice.recursionproblems;
+import java.util.*;
+/**Problem statement:
+ Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+ An integer n is a power of two, if there exists an integer x such that n == 2x.
+ Example 1:
+
+ Input: n = 1
+ Output: true
+ Explanation: 20 = 1
+ Example 2:
+
+ Input: n = 16
+ Output: true
+ Explanation: 24 = 16
+
+ Leetcode link: https://leetcode.com/problems/power-of-two/description/
+ **/
+
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        if (n % 2 != 0) {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+    }
+}
