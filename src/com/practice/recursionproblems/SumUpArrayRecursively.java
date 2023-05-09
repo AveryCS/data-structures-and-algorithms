1a3dbcbb894ea47209167bed2877d2cd6c6d51c9
AveryCS <averydcs@gmail.com>
2023-05-09 12:50:55 -0400

    Adding additional notes to sum up array recursively, and changing to a clearer base case

--- src/com/practice/recursionproblems/SumUpArrayRecursively.java
+++ src/com/practice/recursionproblems/SumUpArrayRecursively.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**Problem statement:
- Given an array of ints, sum up the array recursively **/
+ Given an array of ints, sum up the array using tail recursion**/
 
 public class SumUpArrayRecursively {
 
@@ -13,9 +13,15 @@ public class SumUpArrayRecursively {
         System.out.print("The sum of the array is "+ sumArray(nums,0) +".");
 
     }
-
+    /**The implementation of this method includes tail recursion, meaning that the recursive call is at the tail end/
+     * no additional operations are being done after the recursive call.
+     *
+     * A helpful way to think about this is that in this call, I'm adding the element that I'm on (nums[index]) and
+     * trusting the recursive call to sum up the remainder of the array.
+     *
+     * */
     public static int sumArray( int[] nums, int index){
-        if(index >= nums.length){
+        if(index == nums.length){
             return 0;
         }
 
