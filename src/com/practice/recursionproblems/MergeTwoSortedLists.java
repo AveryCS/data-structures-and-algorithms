d92719f2bd8a453552892141e60a7834d00a898a
AveryCS <averydcs@gmail.com>
2023-06-13 15:51:00 -0400

    Adding recursive solution for Merge Two Sorted Lists

--- src/com/practice/recursionproblems/MergeTwoSortedLists.java
+++ src/com/practice/recursionproblems/MergeTwoSortedLists.java
@@ -28,45 +28,36 @@ import java.util.*;
  **/
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result;
-        if (list1 == null) {
-            return list2;
+        return mergeTheTwoLists(list1, list2);
+    }
+    public static ListNode mergeTheTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
         }
-        if (list2 == null) {
+        if (list1 != null && list2 == null) {
             return list1;
         }
-        if (list1.val < list2.val) {
-            result = list1;
-            list1 = list1.next;
-        } else {
-            result = list2;
-            list2 = list2.next;
+        if (list2 != null && list1 == null) {
+            return list2;
         }
 
-        ListNode temp = result;
-        while (list1 != null && list2 != null) {
+        if (list1.next == null && list2.next == null) {
             if (list1.val < list2.val) {
-                temp.next = list1;
-                list1 = list1.next;
-
+                list1.next = list2;
+                return list1;
             } else {
-                temp.next = list2;
-                list2 = list2.next;
-
+                list2.next = list1;
+                return list2;
             }
-            temp = temp.next;
         }
-        while (list1 != null) {
-            temp.next = list1;
-            list1 = list1.next;
-            temp = temp.next;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTheTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTheTwoLists(list1, list2.next);
+            return list2;
         }
-        while (list2 != null) {
-            temp.next = list2;
-            list2 = list2.next;
-            temp = temp.next;
-        }
-        return result;
     }
 }
 
