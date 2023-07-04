0b502b5bff84d90dda499391fde8d4db7cef4bd2
AveryCS <averydcs@gmail.com>
2023-07-04 15:42:38 -0400

    Adding solution for Missing Number

---
+++ src/com/practice/miscellaneous/MissingNumber.java
@@ -0,0 +1,39 @@
+package com.practice.miscellaneous;
+import java.util.*;
+/**Problem statement:
+ Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ Example 1:
+ Input: nums = [3,0,1]
+ Output: 2
+ Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+ Example 2:
+ Input: nums = [0,1]
+ Output: 2
+ Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+ Example 3:
+
+ Input: nums = [9,6,4,2,3,5,7,0,1]
+ Output: 8
+ Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+ Constraints:
+ n == nums.length
+ 1 <= n <= 104
+ 0 <= nums[i] <= n
+ All the numbers of nums are unique.
+ Leetcode link: https://leetcode.com/problems/missing-number/description/
+ **/
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i =0; i<= nums.length;i++){
+            q.add(i);
+        }
+        for(int i: nums){
+            q.remove(i);
+        }
+        return q.remove();
+    }
+}
